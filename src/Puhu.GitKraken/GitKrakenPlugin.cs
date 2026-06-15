f04f64a6e7c99266c311bb6a8001d53d269589c9
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-06-15 07:38:58 +0200

    feat: wire up full GitKraken plugin registration with actors, services, and routes

--- src/Puhu.GitKraken/GitKrakenPlugin.cs
+++ src/Puhu.GitKraken/GitKrakenPlugin.cs
@@ -1,3 +1,10 @@
+using Akka.Actor;
+using Akka.DependencyInjection;
+using Akka.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Puhu.GitKraken.Actors;
+using Puhu.GitKraken.Pages;
+using Puhu.GitKraken.Services;
 using Puhu.Plugin;
 
 namespace Puhu.GitKraken;
@@ -8,6 +15,33 @@ public sealed class GitKrakenPlugin : IPuhuPlugin
 
     public void Configure(IPuhuPluginBuilder builder)
     {
-        builder.WithTab("Git", "/git");
+        var repoPath = Directory.GetCurrentDirectory();
+
+        builder
+            .WithTab("Git", "/git")
+            .WithServices(services =>
+            {
+                services.AddSingleton<GraphRenderer>();
+                services.AddSingleton(new GitRepoSettings(repoPath));
+            })
+            .WithActors((system, registry, resolver) =>
+            {
+                var settings = resolver.GetService<GitRepoSettings>();
+                var actor = system.ActorOf(
+                    Props.Create(() => new GitRepoActor(settings.RepoPath)),
+                    "gitkraken");
+                registry.Register<GitRepoActor>(actor);
+
+                var tickRouter = registry.Get<TickRouterKey>();
+                tickRouter.Tell(new RegisterMonitor(
+                    "gitkraken", actor, false, TimeSpan.FromSeconds(30)));
+            })
+            .WithRoutes(termina =>
+            {
+                termina.RegisterRoute<GraphPage, GraphViewModel>("/git");
+                termina.RegisterRoute<CommitDetailPage, CommitDetailViewModel>("/git/commit/{hash}");
+            });
     }
 }
+
+public sealed record GitRepoSettings(string RepoPath);
